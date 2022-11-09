28c2ff892819e6a280df564e21b95eade4a8499e
adit313 <app2116@gmail.com>
2022-11-09 14:22:02 -0500

    Added Chainlink Co2 route

--- routes.jl
+++ routes.jl
@@ -1,4 +1,4 @@
-using Genie.Router
+using Genie, Genie.Requests, Genie.Renderer.Json
 using Mimi
 using MimiFUND
 using Dates
@@ -116,4 +116,31 @@ route("/") do
     pulse_size = parse(Float64, params(:pulse_size, "10000000"))
 
     scc = MimiFUND.compute_scsf6(m, year = input_year, eta = input_eta, prtp = input_prtp, last_year = input_last_year, pulse_size = pulse_size)
+  end
+
+  route("/chainlink/co2", method = POST) do
+    # Get the Social Cost of CO2 in year 2020 from the default MimiFUND model:
+    m = MimiFUND.get_model()
+    @show jsonpayload()
+
+    #Input Year
+    input_year = parse(Int64, jsonpayload()["data"]["year"])
+    @show input_year
+
+    #input prtp
+    input_prtp = parse(Float64, jsonpayload()["data"]["discount_rate"])
+    @show input_prtp
+
+    #input eta
+    input_eta = parse(Float64, params(:eta, "0"))
+
+    #input last_year
+    input_last_year = parse(Int64, params(:last_year, "3000"))
+
+    #input pulse_size
+    pulse_size = parse(Float64, params(:pulse_size, "10000000"))
+
+    scc = MimiFUND.compute_scco2(m, year = input_year, eta = input_eta, prtp = input_prtp, last_year = input_last_year, pulse_size = pulse_size)
+    @show scc
+    "{\"jobRunID\":0,\"data\":{\"result\":$(scc)},\"result\":$(scc),\"statusCode\":200}"
   end
